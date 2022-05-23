28c02ac61a9372fb713fdce03882f5a780b58b2e
birbwatcher <d@rkmind.cc>
2022-05-23 12:18:57 +0200

    feat: 03 files in folder implementation

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+async function getDirInfo() {
+    let readDir = await fsPromises.readdir(path.join(__dirname, 'secret-folder'),{withFileTypes: true});
+    let onlyFiles = readDir.filter(dirent => !dirent.isDirectory());
+    let sizes = [];
+    onlyFiles.forEach((el, i) => {
+        let fileExt = path.extname(onlyFiles[i].name.toString());
+        let fileName = path.basename(onlyFiles[i].name.toString(), path.extname(onlyFiles[i].name.toString()));
+            fs.stat(path.join(__dirname + '\\secret-folder', onlyFiles[i].name.toString()), (error, stats) => {
+            if (error) {
+              console.log(error);
+            }
+            if (stats) {
+               sizes.push(Math.ceil(stats.size/1024)+ 'kB')
+               const actualPromise = fs.promises.stat(path.join(__dirname + '\\secret-folder', onlyFiles[i].name.toString()), (error, stats) => {
+                if (error) {
+                  console.log(error);
+                }
+                if (stats) {
+                   sizes.push(Math.ceil(stats.size/1024)+ 'kB')
+                }
+            }).then(() => console.log(`${fileName} - ${fileExt.slice(1)} - `+ sizes[i]));
+            }
+        });
+    })
+}
+
+
+
+getDirInfo()
